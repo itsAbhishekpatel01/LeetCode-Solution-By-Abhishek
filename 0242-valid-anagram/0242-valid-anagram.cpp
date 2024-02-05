d41f7b12bc0c92bd91dbbee7e6e8f0d4461a80c4
Abhishek Patel <144606330+itsAbhishekpatel01@users.noreply.github.com>
2024-02-05 12:45:41 +0530

    Time: 11 ms (42.51%), Space: 9.8 MB (5.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>m1,m2;
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
+            m1[s[i]]++;
+            m2[t[i]]++;
+        }
+        return m1==m2;
+    }
+};
