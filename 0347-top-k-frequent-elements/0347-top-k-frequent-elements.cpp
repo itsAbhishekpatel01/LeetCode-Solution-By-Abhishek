27dab2d425e5774816bc101700ec42b0f01fde2d
Abhishek Patel <144606330+itsAbhishekpatel01@users.noreply.github.com>
2024-02-13 17:43:57 +0530

    Time: 16 ms (34.66%), Space: 16.4 MB (35.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -16,4 +16,14 @@ public:
         }
         return ans;
     }
+    /*
+    My approach:
+    maine do map bnaya hai , pahle map me to har ek number ki frequency store karali hai
+    fir ek dusra multimap liya hai jisme mene phle map ko ulta krke dal diya hai
+    dhyan rahe ki dusra wala multimap hona jaruri hai kyuki ek se jyada elements ki frequency
+    same ho sakti hai
+    ab bas k times multimap ko pichhe se traverse krna hai 
+    use ek vector me dalte jana hai 
+    aur last me usko return kr dena hai
+    */
 };
